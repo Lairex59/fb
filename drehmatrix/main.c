36c91ae85d1d44f790efb31ae329e7f9d62232fa
Lairex59 <laraibahmed2004@gmail.com>
2021-05-03 21:27:08 +0200

    maybe virus just went brr hahhahahah

--- drehmatrix/main.c
+++ drehmatrix/main.c
@@ -3,22 +3,27 @@
 #include "matrix.h"
 
 int main() {
-	matrix_t m1;
-	matrix_t m2;
-	matrix_t a;
-	double winkel = M_PI/2;
+	matrix_t m1, m2;
 	
-	m1.zeilen = 1;
+	double winkel = M_PI/180;
+	
+	m1.zeilen = 2;
 	m1.spalten = 2;
 	m2.zeilen = 2;
-	m2.spalten = 2;
+	m2.spalten = 1;
 	
-	m1.matrix[0][0] = 1;
-	m1.matrix[1][0] = 0;
-	m2.matrix[0][0] = cos(winkel);
-	m2.matrix[0][1] = -sin(winkel);
-	m2.matrix[1][0] = sin(winkel);
-	m2.matrix[1][1] = cos(winkel);
+	m1.matrix[0][0] = cos(winkel);
+	m1.matrix[0][1] = -sin(winkel);
+	m1.matrix[1][0] = sin(winkel);
+	m1.matrix[1][1] = cos(winkel);
 
+	m2.matrix[0][0] = 1;
 
+	m2.matrix[1][0] = 0;
+
+	for (int i = 0; i < 360; i++) {
+		printmatrix(m2);
+		m2 = matrixmult(m1, m2);
+	}
+	return(0);
 }
